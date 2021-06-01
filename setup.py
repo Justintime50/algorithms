d3404b044357c64469f2f053c603d2fc1efdc7c6
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-05-31 18:55:10 -0600

    Pin dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 REQUIREMENTS = [
-    'numpy >= 1.19.4',
+    'numpy == 1.*',
+]
+
+DEV_REQUIREMENTS = [
+    'coveralls == 3.*',
+    'flake8',
+    'mock == 4.*',
+    'pytest == 6.*',
+    'pytest-cov == 2.*',
 ]
 
 setuptools.setup(
@@ -25,13 +33,7 @@ setuptools.setup(
     ],
     install_requires=REQUIREMENTS,
     extras_require={
-        'dev': [
-            'pytest >= 6.0.0',
-            'pytest-cov >= 2.10.0',
-            'coveralls >= 2.1.2',
-            'flake8 >= 3.8.0',
-            'mock >= 4.0.0',
-        ]
+        'dev': DEV_REQUIREMENTS
     },
     python_requires='>=3.6',
 )
