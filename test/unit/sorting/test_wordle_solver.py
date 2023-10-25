2e35da94c6c8bf65fd920970b9a23a34d78af444
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-10-25 12:04:18 -0600

    fix: lint

--- test/unit/sorting/test_wordle_solver.py
+++ test/unit/sorting/test_wordle_solver.py
@@ -11,16 +11,16 @@ def test_get_most_common():
     file_data = _read_file(os.path.join('algorithms', 'assets', 'wordle_answer_list.json'))
     most_common_start, most_common_letters, possible_words = get_most_common(file_data)
 
-    assert type(most_common_start) == list
-    assert type(most_common_letters) == list
-    assert type(possible_words) == list
+    assert isinstance(most_common_start, list)
+    assert isinstance(most_common_letters, list)
+    assert isinstance(possible_words, list)
 
 
 def test_get_best_guess():
     file_data = _read_file(os.path.join('algorithms', 'assets', 'wordle_answer_list.json'))
     best_guess = get_best_guess(file_data)
 
-    assert type(best_guess) == list
+    assert isinstance(best_guess, list)
 
 
 def test_read_file():
